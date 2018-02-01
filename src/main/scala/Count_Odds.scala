c909401a8fdf6dc036eac861e0e3fb48959628ed
Kevin Coatsworth <kevin.coatsworth@digital.hmrc.gov.uk>
2018-02-01 22:38:21 +0000

    Change Count Odds to a function.

--- src/main/scala/Count_Odds.scala
+++ src/main/scala/Count_Odds.scala
@@ -3,6 +3,12 @@ import scala.io.StdIn
 object Count_Odds extends App {
 
   val userNum = StdIn.readLine("Please enter a number: ").toInt
-  print(List.range(1,userNum,2)) //1 to userNum by 2
 
+  listOddNums(userNum)
+
+  def listOddNums(num : Int) {
+
+    print(List.range(1, num, 2)) //1 to userNum by 2
+
+  }
 }
