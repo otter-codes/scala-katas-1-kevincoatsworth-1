484a14b435a1d3a67539c8ed8c61c88ad5e1ccc2
Kevin Coatsworth <kevin.coatsworth@digital.hmrc.gov.uk>
2018-02-05 08:45:50 +0000

    Add Pan's code for learning.

--- src/main/scala/Index_Multiples.scala
+++ src/main/scala/Index_Multiples.scala
@@ -1,16 +1,16 @@
 import scala.io.StdIn
 
-//object Index_Multiples extends App {
-//
-//  val list = (2,4,7,9,10)
-//
-//  returnMultipleOfIndex(List(1,2,3), x => x +2)
-//
-//  def returnMultipleOfIndex(list: List[Int], f: Int => Int): List[Int] = {
-//
-//    val x =  f(list.head) :: returnMultipleOfIndex(list.tail, f)
-//
-//    return x
+object Index_Multiples extends App {
 
-//
-//  }
+  val TheList = List(2, 4, 7, 9, 10,50)
+
+
+  val refined = TheList.zipWithIndex
+    .tail
+    .filter(x => x._1 % x._2 == 0)
+    .map(x => x._1)
+
+  println(refined)
+
+  // code from nick / pan
+}
